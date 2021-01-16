83593d782bc3cf3f28e18950fd19afae38b077fe
Marcos moraes <meuid3@gmail.com>
2021-01-16 02:02:49 -0300

    ADD - Adicionando projetos iniciais

---
+++ Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(subtraction.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                if(ConvertToDecimal(secondNumber) != 0)
+                {
+                    var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                    return Ok(subtraction.ToString());
+                }
+                throw new Exception("O valor não pode ser dividido por (0)!");
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            return double.TryParse(strNumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out number);
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            return decimal.TryParse(strNumber, out decimalValue) ? decimalValue : 0;
+        }
+    }
+}
